465c0769bcb528f2a742a121ebe1b11b28851d18
Geissivan Falcão <geissivan@gmail.com>
2022-10-05 21:56:55 -0300

    Bug Added

--- app.py
+++ app.py
@@ -7,7 +7,17 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "POC DO GEISSIVAN PRA AULA DE DEVOPS DA FIAP"
+    return "POC DO GEISSIVAN PRA AULA DE DEVOPS DA FIAP - 08-AppBug"
+
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
 
 if __name__ == '__main__':
     app.run()
